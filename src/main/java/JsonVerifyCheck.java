707d3a31637b3e4d9c96ab9c11793a5dc7808930
inf141291 <mateusz.ollek@student.put.poznan.pl>
2024-06-01 17:36:39 +0200

    updating change name

--- src/main/java/JsonVerifyCheck.java
+++ src/main/java/JsonVerifyCheck.java
@@ -1,7 +1,6 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.networknt.schema.*;
 import com.networknt.schema.output.OutputUnit;
-import com.networknt.schema.serialization.JsonMapperFactory;
 //import io.vertx.core.json.JsonObject;
 //import io.vertx.json.schema.*;
 import com.google.common.io.Resources;
@@ -12,7 +11,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Set;
 
 //import static io.vertx.json.schema.OutputFormat.Basic;
 
@@ -20,11 +18,17 @@ public class JsonVerifyCheck {
     private static final String id = "https://localhost/";
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        URL datasourceTypesURL = Resources.getResource("schemas/datasource_types.schema.json");
+        URL datasourceTypesURL = Resources.getResource("schemas/flight_asset_descriptor.schema.json");
         String dataSourceTypesString = Resources.toString(datasourceTypesURL, StandardCharsets.UTF_8);
 
-        URL testdatasourceTypesURL = Resources.getResource("test_datasource.json");
+        URL testdatasourceTypesURL = Resources.getResource("test_flight_descriptor.json");
         String testdataSourceTypesString = Resources.toString(testdatasourceTypesURL, StandardCharsets.UTF_8);
+
+//        URL datasourceTypesURL = Resources.getResource("schemas/datasource_types.schema.json");
+//        String dataSourceTypesString = Resources.toString(datasourceTypesURL, StandardCharsets.UTF_8);
+//
+//        URL testdatasourceTypesURL = Resources.getResource("test_datasource.json");
+//        String testdataSourceTypesString = Resources.toString(testdatasourceTypesURL, StandardCharsets.UTF_8);
 
         networkntCheck(dataSourceTypesString, testdataSourceTypesString);
 
@@ -38,18 +42,20 @@ public class JsonVerifyCheck {
 //        System.out.println(result.toJson().encodePrettily());
 //    }
 
-    private static void networkntCheck(String dataSourceTypesString, String testdataSourceTypesString) throws JsonProcessingException {
+    private static void networkntCheck(String schemaString, String jsonString) throws JsonProcessingException {
         JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012);
         SchemaValidatorsConfig config = new SchemaValidatorsConfig();
         config.setPathType(PathType.JSON_POINTER);
+        config.setFormatAssertionsEnabled(true);
         //config.setApplyDefaultsStrategy(new ApplyDefaultsStrategy(true, true, true) );
-        JsonSchema schema = jsonSchemaFactory.getSchema(dataSourceTypesString,config);
-        OutputUnit outputUnit  = schema.validate(testdataSourceTypesString, InputFormat.JSON, OutputFormat.LIST, executionContext -> {
+        JsonSchema schema = jsonSchemaFactory.getSchema(schemaString,config);
+        OutputUnit outputUnit  = schema.validate(jsonString, InputFormat.JSON, OutputFormat.LIST, executionContext -> {
             // By default since Draft 2019-09 the format keyword only generates annotations and not assertions
-            executionContext.getExecutionConfig().setFormatAssertionsEnabled(true);
             executionContext.getExecutionConfig().setAnnotationCollectionEnabled(true);
             executionContext.getExecutionConfig().setAnnotationCollectionFilter(keyword -> true);
         });
+        //List<OutputUnit> outputUnits = outputUnit.getDetails();
+       // outputUnits.sort(Comparator.comparing(OutputUnit::getInstanceLocation));
         System.out.println(outputUnit);
     }
 }
